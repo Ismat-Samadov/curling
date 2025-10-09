53d5952ee7a657fbf45c0b58a785d90e98e8eb7a
ismat-samadov <ismetsemedov@gmail.com>
2025-10-09 16:16:02 +0400

    #15

--- migrate-drop-customers-table.sql
+++
@@ -1,5 +0,0 @@
--- Drop the customers table as it's redundant (we use users table instead)
-DROP TABLE IF EXISTS poster.customers CASCADE;
-
--- This migration cleans up the database by removing the customers table
--- All user information is now stored in the users table
