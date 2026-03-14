18d88fee9e444b9136777972d6e96504e2db63a7
Ismat-Samadov <ismetsemedov@gmail.com>
2026-03-14 16:04:31 +0400

    hard reset

--- migrations/add_admin_role.sql
+++
@@ -1,5 +0,0 @@
--- Add is_admin column to users table
-ALTER TABLE poster.users ADD COLUMN IF NOT EXISTS is_admin BOOLEAN NOT NULL DEFAULT false;
-
--- Create an index for faster admin queries
-CREATE INDEX IF NOT EXISTS idx_users_is_admin ON poster.users(is_admin);
